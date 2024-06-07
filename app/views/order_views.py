06105dde273100de8f29f1f898351f8a88f253c6
조수아 <suacho0724@gmail.com>
2024-06-07 11:38:51 +0900

    ✨ Feat: category 구현하다가 포기..

--- app/views/order_views.py
+++ app/views/order_views.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, jsonify, current_app
+from sqlalchemy import and_
+from sqlalchemy.exc import SQLAlchemyError
+
 from app import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -6,29 +9,27 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 bp_order = Blueprint('order', __name__, url_prefix='/order')
 
 
+# @bp_order.route('/category', methods=['GET'])
+# # def get_categories():
+
+
 @bp_order.route('/list', methods=['GET'])
 @jwt_required()
 def get_order_list():
-    print("발주목록 get")
     # 토큰으로 지점 조회
-    branch_id = get_jwt_identity()
-    print(branch_id)
-    branch = get_branch_by_branch_id(get_jwt_identity())
+    branch = get_branch_by_branch_code(get_jwt_identity())
 
     if branch is None:
         return jsonify({"msg": "지점 정보를 찾을 수 없습니다."}), 404
-    print("지점 존재")
+
     orders = get_orders_by_branch_code(branch.branch_code)
-    print(orders)
+
     order_list = []
     for order in orders:
-        print(order)
         order_details = get_order_detail_by_order_no(order.order_no)
-        print(order_details)
         items = []
         for detail in order_details:
             item = get_item_by_item_no(detail.item_no)
-            print(item)
             item_dict = {
                 'item_nm': item.item_nm,
                 'order_qty': detail.order_qty,
@@ -41,7 +42,7 @@ def get_order_list():
             'items': items
         }
         order_list.append(order_dict)
-    print(order_list)
+
     return jsonify(order_list), 200
 
 
@@ -63,7 +64,7 @@ def get_orders_by_branch_code(branch_code):
     return db.session.execute(query).fetchall()
 
 
-def get_branch_by_branch_id(branch_code):
+def get_branch_by_branch_code(branch_code):
     # BranchList 테이블 참조
     branch_list = current_app.tables['branch_list']
 
