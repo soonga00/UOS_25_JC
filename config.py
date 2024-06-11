49c841c8a9ed3526e1681d6142f7e24d63483330
조수아 <suacho0724@gmail.com>
2024-06-11 20:38:30 +0900

    ✨ Feat: 프론트 2차완성

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db')) # db 접속 주소
-SQLALCHEMY_TRACK_MODIFICATIONS = False
